f60a995995a14a8c22a9be7d88c635ae7b50f569
jetty.guo <jetty.guo@kutesmart.com>
2016-05-30 12:21:01 +0800

    利用ftp传输拿出文件内容 Signed-off-by: jetty.guo <jetty.guo@kutesmart.com>

---
+++ src/main/java/test/com/ftp/FtpUtil.java
@@ -0,0 +1,155 @@
+package test.com.ftp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
+public class FtpUtil {
+
+	private static FTPClient ftp;
+
+	/**
+	 * 获取ftp连接
+	 * 
+	 * @param f
+	 * @return
+	 * @throws Exception
+	 */
+	public static boolean connectFtp(Ftp f) throws Exception {
+		ftp = new FTPClient();
+		boolean flag = false;
+		int reply;
+		if (f.getPort() == null) {
+			ftp.connect(f.getIpAddr(), 21);
+		} else {
+			ftp.connect(f.getIpAddr(), f.getPort());
+		}
+		ftp.login(f.getUserName(), f.getPwd());
+		ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
+		reply = ftp.getReplyCode();
+		if (!FTPReply.isPositiveCompletion(reply)) {
+			ftp.disconnect();
+			return flag;
+		}
+		ftp.changeWorkingDirectory(f.getPath());
+		flag = true;
+		return flag;
+	}
+
+	/**
+	 * 关闭ftp连接
+	 */
+	public  void closeFtp() {
+		if (ftp != null && ftp.isConnected()) {
+			try {
+				ftp.logout();
+				ftp.disconnect();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * @param path
+	 * @return function:读取指定目录下的文件名
+	 * @throws IOException
+	 */
+	public List<String> getFileList(String path) throws ParseException {
+		List<String> fileLists = new ArrayList<String>();
+		// 获得指定目录下所有文件名
+		FTPFile[] ftpFiles = null;
+		try {
+			ftpFiles = ftp.listFiles(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		for (int i = 0; ftpFiles != null && i < ftpFiles.length; i++) {
+			FTPFile file = ftpFiles[i];
+			if (file.isFile()) {
+				System.out.println("文件夹下面的文件=====" + file.getName());
+				fileLists.add(file.getName());
+			} else if (file.isDirectory()) {
+				System.out.println("文件夹名称为=====" + file.getName());
+				fileLists.add(file.getName());
+				//下面的方法是递归迭代方式进行遍历
+		/*		List<String> childLists = getFileList(path + file.getName() + "/");
+				for (String childFileName : childLists) {
+					fileLists.add(childFileName);
+					String fileType = childFileName.substring(childFileName.lastIndexOf(".") + 1,
+							childFileName.length());
+					System.out.println("文件类型为：" + fileType);
+					FtpUtil ftp = new FtpUtil();
+					if (fileType.equals("txt")) {
+						System.out.println("文件名为：" + childFileName);
+						String content = "";
+						content = ftp.readFile(path + file.getName() + "/" + childFileName);
+						System.out.println("文件内容为：" + content);
+					}
+				}*/
+			}
+		}
+		return fileLists;
+	}
+	
+	
+	/**
+	  * @param fileName
+	  * @return function:从服务器上读取指定的文件
+	  * @throws ParseException
+	  * @throws IOException
+	  */
+	public String readFile(String fileName) throws ParseException {
+	  InputStream ins = null;
+	  StringBuilder builder = null;
+	  try {
+	   // 从服务器上读取指定的文件
+	   ins = ftp.retrieveFileStream(fileName);
+	   BufferedReader reader = new BufferedReader(new InputStreamReader(ins, "GBK"));
+	   String line;
+	   builder = new StringBuilder(150);
+	   while ((line = reader.readLine()) != null) {
+	    System.out.println(line);
+	    builder.append(line);
+	   }
+	   reader.close();
+	   if (ins != null) {
+	    ins.close();
+	   }
+	   // 主动调用一次getReply()把接下来的226消费掉. 这样做是可以解决这个返回null问题
+	   ftp.getReply();
+	  } catch (IOException e) {
+	   e.printStackTrace();
+	  }
+	  return builder.toString();
+	}
+
+	public static void main(String[] args) throws Exception {
+		FtpUtil f1 = new FtpUtil();
+
+		Ftp f = new Ftp();
+		f.setIpAddr("192.168.3.199");
+		f.setUserName("ftpadmin");
+		f.setPort(21);
+		f.setPwd("wangzhijie_er3456t8");
+		f.setPath("");
+		boolean connectFtp = FtpUtil.connectFtp(f);
+		if(connectFtp){
+			List<String> fileList = f1.getFileList("update/release/");
+			f1.closeFtp();
+			for (String s : fileList) {
+				System.out.println(s);
+			}
+		}
+	
+	}
+
+}
