b97cec8d55e181925e7a06880ea2b1ad46aece97
jetty.guo <shuaiguo_18@163.com>
2016-06-14 20:26:22 +0800

    解析buffer部分下标所保存的值
    Signed-off-by: jetty.guo <shuaiguo_18@163.com>

--- src/main/java/test/com/handle/Test4.java
+++ src/main/java/test/com/handle/Test4.java
@@ -21,8 +21,8 @@ public class Test4 {
         System.out.println("buf.capacity():"+buf.capacity());//100
         System.out.println("buf.limit():"+buf.limit()); //100
         
-        System.out.println("insert...........................................");
         
+        System.out.println("insert...........................................");
         
 		String strOut = s;
 		buf.putInt(strOut.getBytes(Charset.forName("GBK")).length);//插入141个字节数字长度,从4开始,后续我们需要拿出来......................
@@ -50,19 +50,29 @@ public class Test4 {
         System.out.println("buf.limit():"+buf.limit()); //233 读入时候的position位置; 表示可写入或可读取数据的限制.  limit表示你最多能读到多少数据
         System.out.println("buf.mark():"+buf.markValue());//初始化的时候所在的值  -1  mark值,(mark必须小于position)
         
-        //	buf.mark();          Byffer中，一个不可变的定律是：mark < position <= limit <= capacity。
+        //	buf.mark();          Byffer中，一个不可变的定律是：mark <= position <= limit <= capacity。
         
         System.out.println("get...从buffer拿出来..............................................");
         System.out.println("Returns this buffer's position:"+buf.position());//信息量是从4开始,因为getInt,偏移4个原因
 
         int length =buf.getInt(buf.position());//must smaller than the buffer's limit
-        
+        System.out.println("从以前put过的索引位置为4的下标:length:"+length);//141,4取出来的数值
+
         buf.getInt();  //The int read 不加会截取信息出错  then increments the position by four.  指针又移动四个位置
         System.out.println("buffer's position:"+buf.position());//4+4=8,从8开始取出string
+        System.out.println("buffer's position 4: " +buf.getInt(4)); //第四个索引存放的是整数
+        System.out.println("buffer's position 8:  "+(char)buf.get(8));//t
+        System.out.println("buffer's position 9:  "+(char)buf.get(9));//t
+        System.out.println("buffer's position 10:  "+(char)buf.get(10));//t
+        System.out.println("buffer's position 11: "+(char)buf.get(11));//0
+        
         byte[] bytes = new byte[length]; 
-        buf.get(bytes, 0, length);  
+
+        buf.get(bytes, 0, length);  //0  The position in the original buffer  length:The number of bytes to copy
+        //System.out.println(ioBuffer.position());
+        
+        
         System.out.println("从buffer拿出来的信息:"+ new String(bytes,"GBK"));
-        System.out.println("length:"+length);//141,4~8取出来的数值
         System.out.println(buf.toString());
         
         if(length> buf.remaining() -4){//如果消息内容不够，则重置，相当于不读取size
