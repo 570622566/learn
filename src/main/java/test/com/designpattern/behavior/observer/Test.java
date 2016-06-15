52f33fa5216bfa74c026a5c9fa5d6be95dbac2de
jetty.guo <shuaiguo_18@163.com>
2016-06-15 14:10:39 +0800

    观察者和被观察者
    Signed-off-by: jetty.guo <shuaiguo_18@163.com>

--- src/main/java/test/com/designpattern/behavior/observer/Test.java
+++ src/main/java/test/com/designpattern/behavior/observer/Test.java
@@ -8,13 +8,13 @@ import test.com.designpattern.behavior.observer.role.Watcher;
 public class Test {
 	public static void main(String[] args) {
 		
-        Watched girl = new ConcreteWatched();
-        Watcher watcher1 = new ConcreteWatcher();
+        Watched girl = new ConcreteWatched();//被观察者
+        Watcher watcher1 = new ConcreteWatcher();//观察者
         Watcher watcher2 = new ConcreteWatcher();
         Watcher watcher3 = new ConcreteWatcher();	
 		
         
-        girl.addWatcher(watcher1);
+        girl.addWatcher(watcher1); //被观察者增加观察者
         girl.addWatcher(watcher2);
         girl.addWatcher(watcher3);
         
