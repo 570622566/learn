424be06b85f50841ca1928c1d928b1a9cdfcc143
jetty.guo <shuaiguo_18@163.com>
2016-06-16 00:00:18 +0800

    客户端增加debug级别的日志过滤
    Signed-off-by: jetty.guo <shuaiguo_18@163.com>

--- src/main/java/test/com/mina2/image/client/ImageMinaClient.java
+++ src/main/java/test/com/mina2/image/client/ImageMinaClient.java
@@ -3,6 +3,8 @@ package test.com.mina2.image.client;
 import java.net.InetSocketAddress;
 
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.logging.LogLevel;
+import org.apache.mina.filter.logging.LoggingFilter;
 import org.apache.mina.transport.socket.nio.NioSocketConnector;
 
 import test.com.mina2.image.oper.MyProtocalCodecFactory;
@@ -13,6 +15,11 @@ public class ImageMinaClient {
 	public static void main(String args[])
 	{
 		NioSocketConnector conn=new NioSocketConnector();
+		LoggingFilter lf = new LoggingFilter();
+		lf.setSessionOpenedLogLevel(LogLevel.DEBUG);// 配置日志级别
+
+		// 设置日志记录器
+		conn.getFilterChain().addLast("logger", lf);
 		conn.getFilterChain().addLast("code",new ProtocolCodecFilter(new MyProtocalCodecFactory("utf-8")));
 
 		conn.setHandler(new minaImageHander());
