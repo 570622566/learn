0e512bfef5f396c4847a5cf6d5ea2d7e1301c8ef
jetty.guo <shuaiguo_18@163.com>
2016-06-15 16:09:03 +0800

    code Filter的必要性,一个发送可能多个处理,或者是多个处理可能一个发送; 必须定义消息的开始和结束,防止粘包问题的出现; 必须根据现有的业务实现自己的协议过滤ProtocolCodecFilter;
    Signed-off-by: jetty.guo <shuaiguo_18@163.com>

--- src/main/java/test/com/mina2/MsgCodecFactory.java
+++ src/main/java/test/com/mina2/MsgCodecFactory.java
@@ -5,6 +5,17 @@ import org.apache.mina.filter.codec.ProtocolCodecFactory;
 import org.apache.mina.filter.codec.ProtocolDecoder;
 import org.apache.mina.filter.codec.ProtocolEncoder;
 
+/**
+ * @author Administrator
+ *
+ *在mina中如果没有ProtocolCodecFilter，发送端的 一个IoSession.write(Object message)操作会触发接收端的多个messageReceived(IoSessionsession, Object message) 事件，多个IoSession.write(Object message)操作也可能会导致只触发了一个messageReceived事件，这不就乱套了嘛。
+
+  很多时候我们需要知道当前message的终止位置和下一个message的起始位置。
+
+  所以,分离基础协议逻辑和业务逻辑。
+ *
+ *
+ */
 public class MsgCodecFactory implements ProtocolCodecFactory {
 	
 	
