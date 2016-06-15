26b5c99c10724c6d12a32ec09d579652b0864c14
jetty.guo <shuaiguo_18@163.com>
2016-06-15 16:05:12 +0800

    TCP保证了按顺序传输所有的数据包，但是不能保证发送端进行了一个写操作会导致接收端相应的进行一个读操作。
    Signed-off-by: jetty.guo <shuaiguo_18@163.com>

--- src/main/java/test/com/mina2/MsgResponseEncoder.java
+++ src/main/java/test/com/mina2/MsgResponseEncoder.java
@@ -8,14 +8,25 @@ import org.apache.mina.filter.codec.ProtocolEncoderAdapter;
 import org.apache.mina.filter.codec.ProtocolEncoderOutput;
 
 public class MsgResponseEncoder extends ProtocolEncoderAdapter {
+	/**
+	 * 
+	 * 一般来说我们如果想从一长串字节流中得到我们要的数据并组织成业务上的pojo，我们一般用以下几种方法：
 
+		1、采用固定长度的message  浪费内存空间,不推荐
+
+		2、用固定的头标示body的长度   适合自定义的编解码器做为过滤
+
+		3、用基于文本的标示，如换行、回车等    mina自带的TextLineCodeFiter,已经实现,但是不能存在\r\n的特殊字符
+	 * 
+	 * 
+	 */
 	@Override
 	public void encode(IoSession session, Object message, ProtocolEncoderOutput out) throws Exception {
 	    System.out.println(message);
 	    IoBuffer buf = IoBuffer.allocate(100).setAutoExpand(true);
 	    
 	    String strOut = message.toString();
-	    buf.putInt(strOut.getBytes(Charset.forName("GBK")).length);
+	    buf.putInt(strOut.getBytes(Charset.forName("GBK")).length);//现在开始的四个字节中存放了,当前信息的长度
 	    buf.putString(strOut,Charset.forName("GBK").newEncoder());
 	    buf.flip();
 	    out.write(buf);
