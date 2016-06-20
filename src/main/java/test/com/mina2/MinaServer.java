c04047e6025820f5eacee5c9ef99c2be7dbdd4c7
jetty.guo <shuaiguo_18@163.com>
2016-06-20 16:34:09 +0800

    活跃性心跳测试 当读取通道空闲的时候发送心跳请求，一旦该心跳请求被发送，那么需要在keepAliveRequestTimeout时间内接收到心跳反馈
    Signed-off-by: jetty.guo <shuaiguo_18@163.com>

--- src/main/java/test/com/mina2/MinaServer.java
+++ src/main/java/test/com/mina2/MinaServer.java
@@ -27,13 +27,19 @@ public class MinaServer {
 
 	private static final int PORT = 3001;// 定义监听端口
 	/** 30秒后超时 */
-	private static final int IDELTIMEOUT = 30;
+	private static final int IDELTIMEOUT = 60;
 	/** 15秒发送一次心跳包 */
-	private static final int HEARTBEATRATE = 90;
+	private static final int HEARTBEATRATE = 15;
 	private static final String HEARTBEATREQUEST = "0x11";
 	private static final String HEARTBEATRESPONSE = "0x12";
 	private static final Logger LOG = LoggerFactory.getLogger(MinaServer.class);
+    private static IoAcceptor acceptor = new NioSocketAcceptor(); // 创建非阻塞的socket传输Acceptor
 
+	
+	public static IoAcceptor getInstance(){
+		return acceptor;
+	}
+	
 	public static void main(String[] args) throws Exception {
 		/**
 		 * 
@@ -45,11 +51,11 @@ public class MinaServer {
 		 * AprSocketAcceptor : 基于apr的阻塞传输Acceptor
 		 * VmPipeSocketAcceptor : 虚拟机内的 Acceptor
 		 */
-		IoAcceptor acceptor = new NioSocketAcceptor(); // 创建非阻塞的socket传输Acceptor
+	
 
 		// acceptor.getSessionConfig().setReadBufferSize(1024);
-		// acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE,
-		// IDELTIMEOUT);
+		 acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE,
+		 IDELTIMEOUT);//30s后,设定session进入空闲状态
 		LoggingFilter lf = new LoggingFilter();
 		lf.setSessionOpenedLogLevel(LogLevel.DEBUG);// 配置日志级别
 
@@ -57,23 +63,19 @@ public class MinaServer {
 		acceptor.getFilterChain().addLast("logger", lf);
 
 		// 设置编码过滤器
-		acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new MsgCodecFactory()));
-		// acceptor.getFilterChain().addLast("codec",new ProtocolCodecFilter(new
-		// TextLineCodecFactory(Charset.forName("UTF-8"))));
+		//acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new MsgCodecFactory()));
+		 acceptor.getFilterChain().addLast("codec",new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
 
 		KeepAliveMessageFactory heartBeatFactory = new KeepAliveMessageFactoryImpl();
 		KeepAliveRequestTimeoutHandler heartBeatHandler = new KeepAliveRequestTimeoutHandlerImpl();
-
 		KeepAliveFilter heartBeat = new KeepAliveFilter(heartBeatFactory, IdleStatus.BOTH_IDLE, heartBeatHandler);
 		// 设置是否forward到下一个filter
-		heartBeat.setForwardEvent(true);
+		heartBeat.setForwardEvent(false);
 		// 设置心跳频率
-		heartBeat.setRequestInterval(HEARTBEATRATE); // 本服务器为被定型心跳
-														// 即需要每10秒接受一个心跳请求
-														// 否则该连接进入空闲状态
-														// 并且发出idled方法回调
-		heartBeat.setRequestTimeout(120);
-		// acceptor.getFilterChain().addLast("heartbeat", heartBeat);
+		heartBeat.setRequestInterval(HEARTBEATRATE); // 本服务器为被定型心跳 即需要每15秒接受一个心跳请求  否则该连接进入空闲状态 并且发出idled方法回调
+		
+		heartBeat.setRequestTimeout(20); //超过该时间后则调用KeepAliveRequestTimeoutHandler.CLOSE 
+		 acceptor.getFilterChain().addLast("heartbeat", heartBeat);
 
 		// 指定业务逻辑处理器
 		ServerHandler serverHandler = new ServerHandler();
@@ -87,8 +89,7 @@ public class MinaServer {
 		System.out.println("Server started on port： " + PORT);
 
 		Thread.sleep(60000l);
-
-		// serverHandler.sendMessage("hiiiiiiiiiiiiiiiiiiiiiiiiii!!");
+		// serverHandler.sendMessage("High!!");
 
 	}
 
@@ -96,60 +97,73 @@ public class MinaServer {
 
 		@Override
 		public void keepAliveRequestTimedOut(KeepAliveFilter filter, IoSession session) throws Exception {
-			filter.setRequestTimeoutHandler(DEAF_SPEAKER);
+            System.out.println("《*服务器端*》心跳包发送超时处理(及长时间没有发送（接受）心跳包)"); 
+
+			filter.setRequestTimeoutHandler(CLOSE);
 		}
 
 	}
 
 	private static class KeepAliveMessageFactoryImpl implements KeepAliveMessageFactory {
+		
 
-		@Override
-		public boolean isRequest(IoSession session, Object message) {// 2
-																		// 判断是否心跳请求包
-																		// 是的话返回true
-			System.out.println("isRequest:" + message);
-			if (message.equals(HEARTBEATREQUEST)) {
-				System.out.println("请求心跳包信息: " + message);
-				return true;
-			}
-			return false;
-		}
-
-		@Override
-		public boolean isResponse(IoSession session, Object message) {// 3
-																		// 由于被动型心跳机制，没有请求当然也就不关注反馈
-																		// 因此直接返回false
-			// TODO Auto-generated method stub
-			System.out.println("isResponse:" + message);
-			if (message.equals(HEARTBEATRESPONSE)) {
-				System.out.println("客户端响应心跳包信息: " + message);
-				return true;
-			}
-
-			return false;
-		}
-
+		/* 返回给客户端的心跳包数据 return 返回结果才是客户端收到的心跳包数据 
+		 */
 		@Override
 		public Object getRequest(IoSession session) { // 1 被动型心跳机制无请求 因此直接返回null
 			// TODO Auto-generated method stub
 			System.out.println("getRequest");
 			LOG.info("请求预设信息: " + HEARTBEATREQUEST);
 			System.out.println("请求预设信息: " + HEARTBEATREQUEST);
+			
 			return HEARTBEATREQUEST;
 		}
+		
 
+		/* 
+		 * 接受到的客户端数据包
+		 */
 		@Override
-		public Object getResponse(IoSession session, Object request) {// 4
-																		// 根据心跳请求request
-																		// 反回一个心跳反馈消息
-																		// non-nul
-			System.out.println("getResponse");
+		public Object getResponse(IoSession session, Object request) {
+			System.out.println("getResponse..."+request);
+			
 			LOG.info("响应预设信息: " + HEARTBEATRESPONSE);
 			/** 返回预设语句 */
 			System.out.println("响应预设信息: " + HEARTBEATRESPONSE);
-			// return HEARTBEATRESPONSE;
-			return null;
+			return HEARTBEATRESPONSE;
 		}
+		
+		
+		/**
+		 * 判断是否是客户端发送来的的心跳包此判断影响 KeepAliveRequestTimeoutHandler实现类 
+         * 判断是否心跳包发送超时 
+		 */
+		@Override
+		public boolean isRequest(IoSession session, Object message) {
+			System.out.println("isRequest:" + message);
+			if (message.equals(HEARTBEATREQUEST)) {
+				System.out.println("接收到客户端心数据包引发心跳事件,心跳数据包是》 " + message);
+				return true;
+			}
+			return false;
+		}
+		
+		
+		/**
+		 * 判断发送信息是否是心跳数据包此判断影响 KeepAliveRequestTimeoutHandler实现类   判断是否心跳包发送超时
+		 */
+		@Override
+		public boolean isResponse(IoSession session, Object message) {
+			System.out.println("isResponse:" + message);
+			if (message.equals(HEARTBEATRESPONSE)) {
+                System.out.println("服务器发送数据包中引发心跳事件: " + message); 
+				return true;
+			}
+
+			return false;
+		}
+
+
 
 	}
 }
