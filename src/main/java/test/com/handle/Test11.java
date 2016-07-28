05d71e3c8091314bf95c0e86b61a84006981d9ac
noah_pc <shuaiguo_18@163.com>
2016-07-28 10:17:27 +0800

    带有引号和,的进行json数组里面的json进行处理

--- src/main/java/test/com/handle/Test11.java
+++ src/main/java/test/com/handle/Test11.java
@@ -1,5 +1,7 @@
 package test.com.handle;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 public class Test11 {
 
@@ -7,5 +9,15 @@ public class Test11 {
 		
 		String s = "38&{\"IP\":\"10.49.138.176\",\"Begin\":\"2016-07-26 11:11:13\",\"End\":\"2016-07-26 11:16:14\",\"Record\":[{\"Name\":\"System\",\"Up\":1237,\"Down\":2664},{\"Name\":\"VRVEDP_M\",\"Up\":3544,\"Down\":21885},{\"Name\":\"PoliceAcessClient\",\"Up\":725,\"Down\":4647},{\"Name\":\"fsshclient\",\"Up\":1311,\"Down\":14848},{\"Name\":\"iexplore\",\"Up\":34,\"Down\":266},{\"Name\":\"WinStop\",\"Up\":120,\"Down\":2670},{\"Name\":\"GAToolBox\",\"Up\":1482,\"Down\":1642},{\"Name\":\"WinStop\",\"Up\":1268,\"Down\":7453},{\"Name\":\"vrvrf_c\",\"Up\":6966,\"Down\":3540}]}";
 		System.out.println(s.substring(3));
+		
+		String s1 = "[{'name':'腾讯QQ'},]";
+		JSONArray jsonArray = JSONArray.parseArray(s1);
+		JSONObject object = (JSONObject) jsonArray.get(0);
+		System.out.println(object.get("name"));
+		
+		net.sf.json.JSONArray jsonArray1 = net.sf.json.JSONArray.fromObject(s1);
+		net.sf.json.JSONObject jsonObject = jsonArray1.getJSONObject(0);
+		
+		System.out.println(jsonObject.get("name"));
 	}
 }
