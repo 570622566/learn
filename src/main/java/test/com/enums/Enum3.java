5bd6b924e683970880305a4de073712e7a1567b9
jetty.guo <shuaiguo_18@163.com>
2016-08-03 20:10:32 +0800

    枚举类的case使用
    Signed-off-by: jetty.guo <shuaiguo_18@163.com>

---
+++ src/main/java/test/com/enums/Enum3.java
@@ -0,0 +1,39 @@
+package test.com.enums;
+
+public class Enum3 {
+  Enum1 degree;
+  
+  public Enum3(Enum1 degree) {
+		this.degree = degree;
+	}
+  
+  public void describe() {
+		System.out.println("this Enum1 is");
+		switch (degree) {
+		case NOT:
+			System.out.println("not spicy at all");
+			break;
+		case MILD:
+		case MEDIUM:
+			System.out.println("a little hot");
+			break;
+		case HOT:
+		case FLAMING:
+			System.out.println("maybe too hot");
+			break;
+		default:
+			break;
+		}
+	}
+  
+  public static void main(String[] args) {
+		Enum3 plain = new Enum3(Enum1.NOT);
+		Enum3 greenChile = new Enum3(Enum1.MEDIUM);
+		Enum3 jalapeno = new Enum3(Enum1.HOT);
+
+		plain.describe();
+		greenChile.describe();
+		jalapeno.describe();
+	}
+  
+}
