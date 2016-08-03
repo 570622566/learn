fbb292aac1f2b7f541945602138532817a21ce4f
noah_pc <shuaiguo_18@163.com>
2016-08-03 16:52:06 +0800

    优秀代码阅读
    Signed-off-by: noah_pc <shuaiguo_18@163.com>

--- src/main/java/test/com/mianshi/Interview.java
+++ src/main/java/test/com/mianshi/Interview.java
@@ -1,5 +1,8 @@
 package test.com.mianshi;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -37,12 +40,111 @@ public class Interview {
 	        System.out.println("count should be: "+10000000+", actual be: "+count);
 	        System.out.println("4. volatile不能保证线程安全");
 	    }
+	   
+	   private static void five(){
+	        ArrayList<Integer> list = new ArrayList<>(20);
+	        list.add(1);
+	        System.out.println("debug code, not execute grow method");
+	        System.out.println("5. list grow 0 times");
+	    }
+	   
+	   private static void six() {
+	        System.out.println("BufferedReader's constructor only accepts a Reader instance");
+	        System.out.println("6. new BufferedReader(new FileInputStream(\"a.dat\")); is wrong");
+	    }
+	   
+	   private static void seven() {
+	        try{
+	            if(true){
+	                throw new IOException();
+	            }
+	        }catch (FileNotFoundException e){
+	            System.out.print("FileNotFoundException!");
+	        }catch (IOException e){
+	            System.out.print("IOException!");
+	        }catch (Exception e){
+	            System.out.print("Exception!");
+	        }
+	        System.out.println("\n7. IOException!");
+	    }
+	   
+	   private static void eight() {
+	        System.out.println("String s;System.out.println(s); error: variable s might not have been initialized\nRecompile with -Xlint:unchecked for details.");
+	        System.out.println("8. 由于String s没有初始化, 代码不能编译通过");
+	    }
+	   
+	   private static void nine() {
+	        System.out.println("5"+2);
+	        System.out.println("9. 52");
+	    }
+	   
+	   private static void ten() {
+	        int i = 2;
+	        int result = 0;
+	        switch(i){
+	            case  1:
+	                result = result + i;
+	            case  2:
+	                result = result + i * 2;
+	            case  3:
+	                result = result + i * 3;
+	        }
+	        System.out.println("result="+result);
+	        System.out.println("10. 10");
+	    }
+	   
+	   private static class Null{
+	        public static void hello(){
+	            System.out.println("hello");
+	        }
+	        public static void main(String[] args) {
+	            ((Null)null).hello();
+	            Null _null = (Null)null;
+	            _null.hello();
+	        }
+	    }
+	   
+	   private static class HelloA{
+	        public HelloA(){
+	            System.out.println("HelloA");
+	        }
+
+	        { System.out.println("I'm A class"); }
+
+	        static {
+	            System.out.println("static A");
+	        }
+	    }
+	   
+	   private static class HelloB extends HelloA{
+	        public HelloB(){
+	            System.out.println("HelloB");
+	        }
+
+	        { System.out.println("I'm B class"); }
+
+	        static {
+	            System.out.println("static B");
+	        }
+
+	        public static void main(String[] args) {
+	            System.out.println("main start");
+	            new HelloB();
+	            new HelloB();
+	            System.out.println("main end");
+	        }
+	    }
+	   
 	 public static void main(String[] args) {
 		 one();
 		 two();
 		 three();
 		// four();
-		 
-		 
+		 five();
+		 six();
+		 seven();
+		 eight();
+		 nine();
+		 ten();
 	}
 }
