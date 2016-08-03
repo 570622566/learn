e67f5ed2994021925b661b932da8ac447c0a2dc2
noah_pc <shuaiguo_18@163.com>
2016-08-03 17:25:41 +0800

    枚举类的测试
    Signed-off-by: noah_pc <shuaiguo_18@163.com>

---
+++ src/main/java/test/com/enums/Enum1.java
@@ -0,0 +1,5 @@
+package test.com.enums;
+
+public enum Enum1 {
+	NOT, MILD, MEDIUM, HOT, FLAMING
+}	//将class关键字换成enum
---
+++ src/main/java/test/com/enums/SimpleEnumUse.java
@@ -0,0 +1,8 @@
+package test.com.enums;
+
+public class SimpleEnumUse {
+	public static void main(String[] args) {
+		Enum1 e1 = Enum1.MEDIUM;
+		System.out.println(e1);
+	}
+}
