dafed1ef227fdee439e7a8968a737fa46e4ccca7
guoshuai <shuaiguo_18@163.com>
2017-10-13 00:23:03 +0800

    fork and join并发

---
+++ src/main/java/test/com/concurrent/ForkJoinPoolTest.java
@@ -0,0 +1,20 @@
+package test.com.concurrent;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+
+public class ForkJoinPoolTest {
+	  /** 
+     * @param args 
+     * @throws Exception 
+     */  
+    public static void main(String[] args) throws Exception {  
+        // 创建包含Runtime.getRuntime().availableProcessors()返回值作为个数的并行线程的ForkJoinPool  
+        ForkJoinPool forkJoinPool = new ForkJoinPool();  
+        // 提交可分解的PrintTask任务  
+        forkJoinPool.submit(new PrintTask(0, 200));  
+        forkJoinPool.awaitTermination(2, TimeUnit.SECONDS);//阻塞当前线程直到 ForkJoinPool 中所有的任务都执行结束  
+        // 关闭线程池  
+        forkJoinPool.shutdown();  
+    }  
+}
---
+++ src/main/java/test/com/concurrent/PrintTask.java
@@ -0,0 +1,39 @@
+package test.com.concurrent;
+
+import java.util.concurrent.RecursiveAction;
+
+//RecursiveAction为ForkJoinTask的抽象子类，没有返回值的任务  
+public class PrintTask extends RecursiveAction {
+	
+	 // 每个"小任务"最多只打印50个数  
+    private static final int MAX = 50;  
+	
+    private int start;  
+    private int end;  
+    
+    PrintTask(int start, int end) {  
+        this.start = start;  
+        this.end = end;  
+    }  
+    
+	@Override
+	protected void compute() {
+		// TODO Auto-generated method stub
+		//当end-start的值小于max时候,开始打印
+		
+		if((end-start)<MAX){
+			for(int i = start;i<end;i++){
+				 System.out.println(Thread.currentThread().getName() + "的i值:" + i);  
+			}
+		}else{
+			 // 将大任务分解成两个小任务  
+            int middle = (start + end) / 2;  
+            PrintTask left = new PrintTask(start, middle);  
+            PrintTask right = new PrintTask(middle, end);  
+            // 并行执行两个小任务  
+            left.fork();  
+            right.fork();  
+		}
+	}
+
+}
