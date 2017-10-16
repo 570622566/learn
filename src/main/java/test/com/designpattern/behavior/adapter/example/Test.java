a29c9e391ed3e27411c4d100afb6af7f3d410a27
guoshuai's PC <shuaiguo_18@163.com>
2017-10-16 16:40:57 +0800

    test

--- src/main/java/test/com/designpattern/behavior/adapter/example/Test.java
+++ src/main/java/test/com/designpattern/behavior/adapter/example/Test.java
@@ -30,10 +30,12 @@ public class Test {
 		
 		
 		GBSocketInterface gbSocket = new GBSocket();  
+		
 		Hotel hotel1 = new Hotel();  
 		// 由于没法充电，我拿出随身带去的适配器，并且将我带来的充电器插在适配器的上端插孔中。这个上端插孔是符合国标的，我的充电器完全可以插进去。
 		SocketAdapter socketAdapter = new SocketAdapter(gbSocket);  
 		hotel1.setSocket(socketAdapter);  
+		
 		hotel1.charge();  
 		
 		
